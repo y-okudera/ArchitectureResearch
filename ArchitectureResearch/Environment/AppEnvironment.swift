727cd17d844b44e261b44d6f9d70c9cb9ed9d95d
yuoku <appledev.yuoku@gmail.com>
2022-08-12 02:50:00 +0900

    Add Core package.

---
+++ ArchitectureResearch/Environment/AppEnvironment.swift
@@ -0,0 +1,25 @@
+//
+//  AppEnvironment.swift
+//  ArchitectureResearch
+//
+//  Created by Yuki Okudera on 2022/08/12.
+//
+
+import Core
+import Domain
+import Infrastructure
+
+final class Environment: AppEnvironment {
+
+    // MARK: - DataSource
+
+    var apiRemoteDataSource: ApiRemoteDataSource {
+        ApiRemoteDataSourceImpl(urlSession: .shared)
+    }
+
+    // MARK: - Repository
+
+    var gitHubRepoRepository: GitHubRepoRepository {
+        GitHubRepoRepositoryImpl(apiRemoteDataSource: self.apiRemoteDataSource)
+    }
+}
---
+++ Packages/Core/Sources/Core/AppEnvironment.swift
@@ -0,0 +1,21 @@
+//
+//  AppEnvironment.swift
+//  
+//
+//  Created by Yuki Okudera on 2022/08/12.
+//
+
+import Domain
+import Foundation
+import Infrastructure
+
+public protocol AppEnvironment {
+
+    // MARK: - DataSource
+
+    var apiRemoteDataSource: ApiRemoteDataSource { get }
+
+    // MARK: - Repository
+
+    var gitHubRepoRepository: GitHubRepoRepository { get }
+}
